4c6e17266b3e083d9b2eb7ad04f4ffd1aa66ac34
vitorismart <vitor@ismartsoftware.com>
2014-12-09 15:44:48 +1000

    test

--- target/client/gscreen.js
+++ target/client/gscreen.js
@@ -16,7 +16,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
  */
 
 (function() {
-  window.GScreen = angular.module("GScreen", ["ng", "ngResource", "ngRoute", "lr.upload"]);
+  window.GScreen = angular.module("GScreen", ["ng", "ngResource", "ngRoute"]);
 
   require("./routes");
 
